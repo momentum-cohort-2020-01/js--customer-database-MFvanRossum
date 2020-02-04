0effe9083ac4aba380647b47f4a533a6c6988af9
MFvanRossum <matissev@hotmail.com>
2020-02-04 16:33:26 -0500

    Addresses display

--- main.js
+++ main.js
@@ -10,8 +10,22 @@ function createNewProfile (customer) {
     img.classList.add('pic')
 
     const name = document.createElement('h2')
-    name.innerText = [customer.name.first + ' ' + customer.name.last]
+    name.innerText = (customer.name.first + ' ' + customer.name.last)
+    name.classList.add('name')
     profile.appendChild(name)
+
+    const email = document.createElement('div')
+    email.innerText = customer.email 
+    email.classList.add('email')
+    profile.appendChild(email)
+
+    const address = document.createElement('div')
+    address.innerHTML = (customer.location.street + '<br>'
+                        + customer.location.city + ', '
+                        + nameToAbbr(customer.location.state) + ', '
+                        + customer.location.postcode)
+    address.classList.add('address')
+    profile.appendChild(address)
 
     return profile
 }
